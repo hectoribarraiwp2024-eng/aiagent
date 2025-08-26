e650112f379048540149c6844f6a4e043b49552b
hectoribarraiwp2024-eng <hector.ibarra.iwp2024@gmail.com>
2025-08-26 05:38:06 -0500

    added conversation funcionality & extra info functionality

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
+from google.genai import types
 
 def main():
     load_dotenv()
+    
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    user_prompt = " ".join(args)
+
+    messages = [types.Content(role='user', parts=[types.Part(text=user_prompt)])]
+
+
     api_key = os.environ.get("GEMINI_API_KEY")
-
     client = genai.Client(api_key=api_key)
-
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+        model='gemini-2.0-flash-001', contents=messages
     )
     
-    print(response.text)
-    print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
-    print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    if '--verbose' in user_prompt:
+        print(f'User prompt: {user_prompt}')
+        print(response.text)
+        print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+        print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    else:
+        print(response.text)
+
 
 
 if __name__ == "__main__":
