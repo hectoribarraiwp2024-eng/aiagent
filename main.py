13cb3f46559e6ebd74a241085ecad988aa1574cb
hectoribarraiwp2024-eng <hector.ibarra.iwp2024@gmail.com>
2026-05-17 17:41:44 -0500

    updated usage metadata loging

--- main.py
+++ main.py
@@ -10,9 +10,20 @@ def main():
     if api_key == None:
         raise Exception("the api_key was not found")
 
-    client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=prompt)
+
+    if response.usage_metadata == None:
+        raise RuntimeError("usage_metadata is None so the API request failed")
+
+    p_tok = response.usage_metadata.prompt_token_count
+    r_tok = response.usage_metadata.candidates_token_count
+    print(f"User prompt: {prompt}")
+    print(f"Prompt tokens: {p_tok}")
+    print(f"Response tokens: {r_tok}")
+    print("Response:")
     print(response.text)
 
 
