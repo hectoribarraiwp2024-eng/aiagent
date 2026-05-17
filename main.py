164f2f6e8712e83d64ebc8f99bfd507b54680442
hectoribarraiwp2024-eng <hector.ibarra.iwp2024@gmail.com>
2026-05-17 17:46:14 -0500

    updated the prompt logic to accept user prompts from the command line

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
-
+import argparse
 
 def main():
     load_dotenv()
@@ -10,7 +10,11 @@ def main():
     if api_key == None:
         raise Exception("the api_key was not found")
 
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    prompt = args.user_prompt
 
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(model="gemini-2.5-flash", contents=prompt)
