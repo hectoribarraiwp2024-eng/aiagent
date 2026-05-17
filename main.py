99c771f8e5392162039b68d53dbdea3981f9e5e5
hectoribarraiwp2024-eng <hector.ibarra.iwp2024@gmail.com>
2026-05-17 17:54:32 -0500

    updated the argument parser to accept a verbose flag that when enabled prints metadata about the response and updated the gerate content logic to accept a list of prompts instead of one prompt to aid with context

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import argparse
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -12,21 +13,27 @@ def main():
 
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     prompt = args.user_prompt
 
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=prompt)
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
 
     if response.usage_metadata == None:
         raise RuntimeError("usage_metadata is None so the API request failed")
 
-    p_tok = response.usage_metadata.prompt_token_count
-    r_tok = response.usage_metadata.candidates_token_count
-    print(f"User prompt: {prompt}")
-    print(f"Prompt tokens: {p_tok}")
-    print(f"Response tokens: {r_tok}")
+    
+    if args.verbose:
+        p_tok = response.usage_metadata.prompt_token_count
+        r_tok = response.usage_metadata.candidates_token_count
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {p_tok}")
+        print(f"Response tokens: {r_tok}")
+    
     print("Response:")
     print(response.text)
 
