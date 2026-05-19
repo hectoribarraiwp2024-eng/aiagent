e110e5ff1534558e8cff69448beab48c929c0a1d
hectoribarraiwp2024-eng <hector.ibarra.iwp2024@gmail.com>
2026-05-18 19:55:15 -0500

    updated the ai to have a feed back loop

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import argparse
 from google.genai import types
 from config import SYSTEM_PROMPT
 from call_function import available_functions, call_function
+import sys
 
 def main():
     load_dotenv()
@@ -21,40 +22,49 @@ def main():
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     prompt = args.user_prompt
+    for i in range(20):
+        client = genai.Client(api_key=api_key)
+        response = client.models.generate_content(
+            model="gemini-2.5-flash", 
+            contents=messages, 
+            config=types.GenerateContentConfig(tools=[available_functions], system_instruction=SYSTEM_PROMPT, temperature=0),
+        )
 
-    client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(
-        model="gemini-2.5-flash", 
-        contents=messages, 
-        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=SYSTEM_PROMPT, temperature=0),
-    )
+        for candidate in response.candidates:
+            messages.append(candidate.content)
 
-    if response.usage_metadata == None:
-        raise RuntimeError("usage_metadata is None so the API request failed")
+        if response.usage_metadata == None:
+            raise RuntimeError("usage_metadata is None so the API request failed")
 
-    if args.verbose:
-        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-        print("Response tokens:", response.usage_metadata.candidates_token_count)
-
-    if not response.function_calls:
-        print("Response:")
-        print(response.text)
-        return
-
-    function_responses = []
-    for function_call in response.function_calls:
-        result = call_function(function_call, args.verbose)
-        if (
-            not result.parts
-            or not result.parts[0].function_response
-            or not result.parts[0].function_response.response
-        ):
-            raise RuntimeError(f"Empty function response for {function_call.name}")
         if args.verbose:
-            print(f"-> {result.parts[0].function_response.response}")
-        function_responses.append(result.parts[0])
+            print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+            print("Response tokens:", response.usage_metadata.candidates_token_count)
 
-    
+        if not response.function_calls:
+            print("Response:")
+            print(response.text)
+            return
+
+        function_responses = []
+        for function_call in response.function_calls:
+            result = call_function(function_call, args.verbose)
+            if (
+                not result.parts
+                or not result.parts[0].function_response
+                or not result.parts[0].function_response.response
+            ):
+                raise RuntimeError(f"Empty function response for {function_call.name}")
+            if args.verbose:
+                print(f"-> {result.parts[0].function_response.response}")
+            function_responses.append(result.parts[0])
+        
+            messages.append(types.Content(role="user", parts=function_responses))
+
+        if i == 19:
+            print('the program already looped 20 times something went wrong')
+            sys.exit(1)
+
+        
 
 if __name__ == "__main__":
     main()
