70b331d3e1c013afe5331b4308ac80eb66cbf8de
hectoribarraiwp2024-eng <hector.ibarra.iwp2024@gmail.com>
2026-09-15 22:18:55 -0500

    created a messages list for future use in ai conversations

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from openai import OpenAI
 
@@ -11,22 +12,25 @@ client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
     api_key=api_key,
 )
-prompt = "Why is Boot.dev such a great place to learn backend development? Use one sentence maximum."
+
+parser = argparse.ArgumentParser(description="chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+
+messages_list = [
+    {"role": "user", "content": args.user_prompt}
+]
+
 response = client.chat.completions.create(
     model="openrouter/free",
-    messages=[
-        {
-            "role": "user",
-            "content": prompt,
-        }
-    ],
+    messages=messages_list,
 )
 
 def main():
     if response.usage == None:
         raise RuntimeError("responses usage property is None. Failed api request")
 
-    print(f"User prompt: {prompt}")
+    print(f"User prompt: {args.user_prompt}")
     print(f"Prompt tokens: {response.usage.prompt_tokens}")
     print(f"Response tokens: {response.usage.completion_tokens}")
     print("Response:")
