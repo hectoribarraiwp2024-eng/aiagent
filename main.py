96f77e4717c7bcdf165a981f124e803f8797bd78
hectoribarraiwp2024-eng <hector.ibarra.iwp2024@gmail.com>
2026-09-15 21:55:34 -0500

    now i can veiw the metadata of the ai response

--- main.py
+++ main.py
@@ -11,18 +11,25 @@ client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
     api_key=api_key,
 )
-
+prompt = "Why is Boot.dev such a great place to learn backend development? Use one sentence maximum."
 response = client.chat.completions.create(
     model="openrouter/free",
     messages=[
         {
             "role": "user",
-            "content": "Why is Boot.dev such a great place to learn backend development? Use one sentence maximum.",
+            "content": prompt,
         }
     ],
 )
 
 def main():
+    if response.usage == None:
+        raise RuntimeError("responses usage property is None. Failed api request")
+
+    print(f"User prompt: {prompt}")
+    print(f"Prompt tokens: {response.usage.prompt_tokens}")
+    print(f"Response tokens: {response.usage.completion_tokens}")
+    print("Response:")
     print(response.choices[0].message.content)
 
 
