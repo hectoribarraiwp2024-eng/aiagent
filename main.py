5ef6791abb28e824104fbb54705ddb6217c9c256
hectoribarraiwp2024-eng <hector.ibarra.iwp2024@gmail.com>
2026-09-15 22:29:16 -0500

    added a verbose flag for input that shows metadata and refactored for better use

--- main.py
+++ main.py
@@ -2,37 +2,40 @@ import os
 import argparse
 from dotenv import load_dotenv
 from openai import OpenAI
-
-load_dotenv()
-api_key = os.environ.get("OPENROUTER_API_KEY")
-if api_key == None:
-    raise RuntimeError("api_key was not generated")
-
-client = OpenAI(
-    base_url="https://openrouter.ai/api/v1",
-    api_key=api_key,
-)
-
-parser = argparse.ArgumentParser(description="chatbot")
-parser.add_argument("user_prompt", type=str, help="User prompt")
-args = parser.parse_args()
-
-messages_list = [
-    {"role": "user", "content": args.user_prompt}
-]
-
-response = client.chat.completions.create(
-    model="openrouter/free",
-    messages=messages_list,
-)
-
 def main():
+    parser = argparse.ArgumentParser(description="chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+    if api_key == None:
+        raise RuntimeError("api_key was not generated")
+
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
+
+    messages_list = [
+        {"role": "user", "content": args.user_prompt}
+    ]
+
+    generate_response(client, messages_list, args)
+
+def generate_response(client, messages_list, args):
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=messages_list,
+    )
+
     if response.usage == None:
         raise RuntimeError("responses usage property is None. Failed api request")
-
-    print(f"User prompt: {args.user_prompt}")
-    print(f"Prompt tokens: {response.usage.prompt_tokens}")
-    print(f"Response tokens: {response.usage.completion_tokens}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage.prompt_tokens}")
+        print(f"Response tokens: {response.usage.completion_tokens}")
     print("Response:")
     print(response.choices[0].message.content)
 
