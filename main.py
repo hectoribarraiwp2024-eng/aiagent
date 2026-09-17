208b07d2cf7e90cd7b500c0d6507f1c7a36ebf75
hectoribarraiwp2024-eng <hector.ibarra.iwp2024@gmail.com>
2026-09-16 21:06:42 -0500

    created the get_files_info schema so the ai agent knows what functions are available for it to call

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 import argparse
+import json
 from dotenv import load_dotenv
 from openai import OpenAI
 from config import system_prompt
+from call_functions import available_functions
 def main():
     parser = argparse.ArgumentParser(description="chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
@@ -30,7 +32,14 @@ def generate_response(client, messages_list, args):
     response = client.chat.completions.create(
         model="openrouter/free",
         messages=messages_list,
+        tools=available_functions,
     )
+
+    message = response.choices[0].message
+    if message.tool_calls:
+        for tool_call in message.tool_calls:
+            function_args = json.loads(tool_call.function.arguments or "{}")
+            print(f"Calling function: {tool_call.function.name}({function_args})")
 
     if response.usage == None:
         raise RuntimeError("responses usage property is None. Failed api request")
